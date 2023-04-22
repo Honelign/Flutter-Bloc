18380fd2a92a384204b719737bf5b8383e2d656b
Honelign <honelignselamu2@gmail.com>
2023-04-22 18:43:55 +0300

    step1 done

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,20 +1,100 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'dart:math' as math show Random;
+import 'dart:developer' as devtools show log;
 
-const names = [
-  'Foo',
-  'Bar',
-  'Baz',
-];
-
-extension RandomElement<T> on Iterable<T> {
-  T getRandomElement() => elementAt(math.Random().nextInt(length));
+@immutable
+abstract class LoadAction {
+  const LoadAction();
 }
 
-class NamesCubit extends Cubit<String?> {
-  NamesCubit() : super(null);
-  void pickRandomNames() => emit(names.getRandomElement());
+@immutable
+class LoadPersonsAction implements LoadAction {
+  final PersonUrl personUrl;
+
+  const LoadPersonsAction({required this.personUrl});
+}
+
+enum PersonUrl { persons1, persons2 }
+
+extension GetPersonUrl on PersonUrl {
+  String get urlString {
+    switch (this) {
+      case PersonUrl.persons1:
+        return 'http://10.0.2.2:5500/api/persons1.json';
+
+      case PersonUrl.persons2:
+        return 'http://10.0.2.2:5500/api/person2.json';
+    }
+  }
+}
+
+@immutable
+class Person {
+  final String name;
+  final int age;
+
+  const Person({required this.name, required this.age});
+  Person.fromJson(Map<String, dynamic> json)
+      : name = json['name'] as String,
+        age = json['age'] as int;
+}
+
+Future<Iterable<Person>> getPersons(String url) => HttpClient()
+    .getUrl(Uri.parse(url))
+    .then((req) => req.close())
+    .then((res) => res.transform(utf8.decoder).join())
+    .then((str) => json.decode(str) as List<dynamic>)
+    .then((list) => list.map((e) => Person.fromJson(e)));
+
+@immutable
+class FetchResult {
+  final Iterable<Person> persons;
+  final bool isRetrievedFromCache;
+
+  const FetchResult(
+      {required this.persons, required this.isRetrievedFromCache});
+  @override
+  String toString() {
+    return "fetch result (isRetrivedFromCashe = $isRetrievedFromCache , persons= $persons)";
+  }
+}
+
+//loggin
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+class PersonsBloc extends Bloc<LoadAction, FetchResult?> {
+  final Map<PersonUrl, Iterable<Person>> _chache = {};
+
+  PersonsBloc() : super(null) {
+    on<LoadPersonsAction>((event, emit) async {
+      final url = event.personUrl;
+
+      if (_chache.containsKey(url)) {
+        final chachedPersons = _chache[url];
+        final result =
+            FetchResult(persons: chachedPersons!, isRetrievedFromCache: true);
+        emit(result);
+      } else {
+        final person = await getPersons(url.urlString);
+        _chache[url] = person;
+        final result =
+            FetchResult(persons: person, isRetrievedFromCache: false);
+        emit(result);
+      }
+    });
+  }
+}
+
+extension Subscript<T> on Iterable<T> {
+  T? operator [](int index) {
+    return length > index ? elementAt(index) : null;
+  }
 }
 
 class HomePage extends StatefulWidget {
@@ -25,52 +105,56 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late NamesCubit cubit;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    cubit = NamesCubit();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    cubit.close();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder<String?>(
-        stream: cubit.stream,
-        builder: (context, snapshot) {
-          final button = TextButton(
-              onPressed: () => cubit.pickRandomNames(),
-              child: Text("Pick a random name"));
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              return button;
-          
-            case ConnectionState.waiting:
-             return button;
-           
-            case ConnectionState.active:
-                return Column(
-                  children: [
-                    Text(snapshot.data ?? ''),
-                    button,
-                  ],
+        body: Padding(
+      padding: const EdgeInsets.only(top: 50.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                  onPressed: () {
+                    context
+                        .read<PersonsBloc>()
+                        .add(LoadPersonsAction(personUrl: PersonUrl.persons1));
+                  },
+                  child: Text("Load 1st Json")),
+              TextButton(
+                  onPressed: () {
+                    context
+                        .read<PersonsBloc>()
+                        .add(LoadPersonsAction(personUrl: PersonUrl.persons2));
+                  },
+                  child: Text("Load 2nd Json"))
+            ],
+          ),
+          BlocBuilder<PersonsBloc, FetchResult?>(
+            buildWhen: (previous, current) {
+              return previous?.persons != current?.persons;
+            },
+            builder: (context, state) {
+              state?.log();
+              final persons = state?.persons;
+              if (persons == null) {
+                return const SizedBox();
+              } else {
+                return Expanded(
+                  child: ListView.builder(
+                      itemCount: persons.length,
+                      itemBuilder: (context, index) {
+                        final person = persons[index]!;
+                        return ListTile(title: Text(person.name));
+                      }),
                 );
-              // TODO: Handle this case.
-             
-            case ConnectionState.done:
-              return const SizedBox();
-             
-          }
-        },
+              }
+            },
+          )
+        ],
       ),
-    );
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'homepage.dart';
 
@@ -18,7 +19,9 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: BlocProvider(
+        create: (_)=>PersonsBloc(),
+        child: const HomePage()),
     );
   }
 }
